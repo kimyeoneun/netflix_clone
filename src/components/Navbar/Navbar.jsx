c33a0e595f66eb95096fd8b8eb33fc650e0801a3
kimyeoneun <kye00150@naver.com>
2025-11-19 00:21:32 +0900

    찐찐막

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-import "./NavBar.css";
+import "./Navbar.css";
 import logo from "../../assets/logo.png";
 import search_icon from "../../assets/search_icon.svg";
 import bell_icon from "../../assets/bell_icon.svg";
