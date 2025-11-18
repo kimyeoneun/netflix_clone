30bc1344afa601f0e64d2fa4ac4706570e7ea666
kimyeoneun <kye00150@naver.com>
2025-11-19 00:13:46 +0900

    찐막

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Home.css'
-import NavBar from '../../components/NavBar/Navbar.jsx'
+import NavBar from '../../components/Navbar/Navbar.jsx'
 import hero_banner from '../../assets/hero_banner.jpg'
 import hero_title from '../../assets/hero_title.png'
 import play_icon from '../../assets/play_icon.png'
